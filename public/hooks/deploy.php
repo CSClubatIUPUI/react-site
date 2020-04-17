2fa3032e006d5be63ced6ada9b8889c8a0e886b3
Alex Hicks <alex@alexhicks.net>
2020-04-17 01:15:14 -0400

    Add message to /, add hooks/deploy.php

---
+++ public/hooks/deploy.php
@@ -0,0 +1,26 @@
+<?php
+$GITHUB_REPO = "CSClubatIUPUI/react-site";
+$OUTPUT_DIR = __DIR__ . "/..";
+$TEMP_ZIP_FILENAME = "/tmp/csclub-react-site.zip";
+
+header("Content-Type: application/json");
+
+$ch = curl_init("https://circleci.com/api/v1.1/project/gh/$GITHUB_REPO/latest/artifacts");
+curl_setopt_array($ch, [
+  CURLOPT_RETURNTRANSFER => true
+]);
+$artifacts = json_decode(curl_exec($ch), true);
+if (count($artifacts) === 0) {
+  http_response_code(404);
+  die(json_encode(["error" => "No artifacts found for latest build"]));
+}
+shell_exec("wget -O $TEMP_ZIP_FILENAME \"{$artifacts[0]["url"]}\"");
+shell_exec("unzip $TEMP_ZIP_FILENAME -d $OUTPUT_DIR");
+shell_exec("mv $OUTPUT_DIR/build/* $OUTPUT_DIR/");
+shell_exec("mv $OUTPUT_DIR/build/.* $OUTPUT_DIR/");
+shell_exec("rm $OUTPUT_DIR/build");
+echo(json_encode([
+  "success" => true,
+  "message" => "Deployed new version of React site successfully"
+]));
+?>
